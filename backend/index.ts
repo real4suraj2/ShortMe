57316f28a04417a9509b36270cb1c93bc18e466e
Suraj Patel <real4suraj@gmail.com>
2022-05-31 23:18:57 +0530

    frontend build and static serve from backend directly

--- backend/index.ts
+++ backend/index.ts
@@ -16,6 +16,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/public'))
 
 app.use('/', router)
 
